bfc611acb0b7bcbcaa3106ef6794b64428feee7a
JSDulai <jsinghdulai@gmail.com>
2022-07-26 14:18:50 +0200

    Content gelöscht

--- Source/Balance3D/Checkpoint.h
+++ Source/Balance3D/Checkpoint.h
@@ -25,7 +25,7 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-		class UBoxComponent * CollisionBox;
+		class UBoxComponent * Collisionbox;
 
 	UFUNCTION()
 		void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
