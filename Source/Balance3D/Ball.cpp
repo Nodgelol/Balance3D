17e618077e22d9005922a265eabcae56f42920fc
Nodgelol <68157210+Nodgelol@users.noreply.github.com>
2022-07-27 14:25:51 +0200

    Revert "Todo"
    This reverts commit d7645914135e0792d0809c2cea60c0d2713b2f32.

--- Source/Balance3D/Ball.cpp
+++ Source/Balance3D/Ball.cpp
@@ -38,6 +38,8 @@ ABall::ABall()
 
 
 	// Others
+	jumping = false;
+	JumpImpulse = 35000.f;
 	MovementSpeed = 600.f;
 }
 
@@ -52,6 +54,9 @@ void ABall::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 	PlayerInputComponent->BindAxis("MoveRight", this, &ABall::MoveRight);
 	PlayerInputComponent->BindAxis("LookUp", this, &ABall::LookUp);
 	PlayerInputComponent->BindAxis("Turn", this, &ABall::Turn);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ABall::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ABall::StopJump);
 }
 
 void ABall::MoveForward(float value)
@@ -86,7 +91,28 @@ void ABall::Turn(float value)
 	AddControllerYawInput(value);
 }
 
+void ABall::Jump()
+{
+	if (!jumping)
+	{
+		// Jump
+		const FVector Impulse = FVector(0.f, 0.f, JumpImpulse);
+		StaticMesh->AddImpulse(Impulse);
+	}
+	jumping = true;
+}
+
+void ABall::StopJump()
+{
+	jumping = false;
+}
+
 void ABall::Teleporte(FVector dahin)
 {
 	StaticMesh->SetWorldLocation(dahin);
+}
+
+void ABall::Rotate(const FQuat &sorum)
+{
+	StaticMesh->SetWorldRotation(sorum);
 }
--- Source/Balance3D/Ball.h
+++ Source/Balance3D/Ball.h
@@ -28,6 +28,12 @@ public:
 		class UCameraComponent* CameraComponent;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool jumping;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float JumpImpulse;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 		float MovementSpeed;
 
 public:
@@ -37,5 +43,8 @@ public:
 	void MoveRight(float value);
 	void LookUp(float value);
 	void Turn(float value);
+	void Jump();
+	void StopJump();
 	void Teleporte(FVector value);
+	void Rotate(const FQuat &value);
 };
