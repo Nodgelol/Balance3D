23bae87e231505bcb6b82b7f055f145e80fd5e3e
Nodgelol <68157210+Nodgelol@users.noreply.github.com>
2022-07-27 14:26:01 +0200

    Revert "Add GameMode Class"
    This reverts commit 8b3dcdba944b6caf76e67782ba6d40987bfd2511.

--- Source/Balance3D/Balance3D_GameMode.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Balance3D_GameMode.h"
-
--- Source/Balance3D/Ball.cpp
+++ Source/Balance3D/Ball.cpp
@@ -35,7 +35,10 @@ ABall::ABall()
 	// Setup Ball Settings
 	StaticMesh->SetSimulatePhysics(true);
 
+
 	// Others
+	jumping = false;
+	JumpImpulse = 35000.f;
 	MovementSpeed = 600.f;
 }
 
@@ -50,6 +53,9 @@ void ABall::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 	PlayerInputComponent->BindAxis("MoveRight", this, &ABall::MoveRight);
 	PlayerInputComponent->BindAxis("LookUp", this, &ABall::LookUp);
 	PlayerInputComponent->BindAxis("Turn", this, &ABall::Turn);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ABall::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ABall::StopJump);
 }
 
 void ABall::MoveForward(float value)
@@ -82,4 +88,20 @@ void ABall::Turn(float value)
 {
 	// Camera Left and Right
 	AddControllerYawInput(value);
+}
+
+void ABall::Jump()
+{
+	if (!jumping)
+	{
+		// Jump
+		const FVector Impulse = FVector(0.f, 0.f, JumpImpulse);
+		StaticMesh->AddImpulse(Impulse);
+	}
+	jumping = true;
+}
+
+void ABall::StopJump()
+{
+	jumping = false;
 }
